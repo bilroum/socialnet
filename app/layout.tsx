2d1eb5a6dda9ddd384460357bac13bbcb63a0c29
bil <bilroum@gmail.com>
2025-11-10 11:35:47 +0100

    FIXED: filename

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/themeProvider";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
